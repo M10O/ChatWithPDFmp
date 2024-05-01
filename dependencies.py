3841d424389c39953abffb9abbca2321f6f29a7b
Vaibhav Kedarisetti <73187017+M10O@users.noreply.github.com>
2024-05-01 19:39:07 +0530

    First

---
+++ dependencies.py
@@ -0,0 +1,213 @@
+import os
+import streamlit as st
+#import streamlit_authenticator as stauth
+import datetime
+import re
+#from deta import Deta
+from dotenv import dotenv_values
+import pymongo
+import bcrypt
+
+config = dotenv_values(".env")
+database_url = config['DATABASE_URL']
+
+def connection():
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+    except Exception as e:
+        print(e)
+
+def insert_user(email, username, password):
+    """
+    Inserts Users into the DB
+    :param email:
+    :param username:
+    :param password:
+    :return User Upon successful Creation:
+    """
+    date_joined = str(datetime.datetime.now())
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+        is_inserted = collection.insert_one({
+            'email': email,
+            'username': username,
+            'password': password, 'date_joined': date_joined
+        })
+        return is_inserted
+    except Exception as e:
+        print(e)
+
+
+def fetch_users():
+    """
+    Fetch Users
+    :return Dictionary of Users:
+    """
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+        users = collection.find()
+        return users
+    except Exception as e:
+        print(e)
+    # users = 
+    # return users.items
+
+def check_in_emails(email):
+    """
+    Check if email exists
+    :param email:
+    :return True if email exists else False:
+    """
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+        user = collection.find_one({'email': email})
+        if user:
+            return False
+        return True
+    except Exception as e:
+        print(e)
+
+def get_user_emails():
+    """
+    Fetch User Emails
+    :return List of user emails:
+    """
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+        users = collection.find()
+        emails = []
+        for user in users.items:
+            emails.append(user['email'])
+        return emails
+    except Exception as e:
+        print(e)
+
+def check_in_usernames(username):
+    """
+    Check if username exists
+    :param username:
+    :return True if username exists else False:
+    """
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+        user = collection.find_one({'username': username})
+        if user:
+            return False
+        return True
+    except Exception as e:
+        print(e)
+
+def get_usernames():
+    """
+    Fetch Usernames
+    :return List of user usernames:
+    """
+    try:
+        client = pymongo.MongoClient(database_url)
+        db = client['DocPlay']
+        collection = db['users']
+        users = collection.find()
+        usernames = []
+        for user in users.items:
+            usernames.append(user['key'])
+        return usernames
+    except Exception as e:
+        print(e)
+
+
+def validate_email(email):
+    """
+    Check Email Validity
+    :param email:
+    :return True if email is valid else False:
+    """
+    pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$" #tesQQ12@gmail.com
+
+    if re.match(pattern, email):
+        return True
+    return False
+
+
+def validate_username(username):
+    """
+    Checks Validity of userName
+    :param username:
+    :return True if username is valid else False:
+    """
+
+    pattern = "^[a-zA-Z0-9]*$"
+    if re.match(pattern, username):
+        return True
+    return False
+
+
+def hash(password: str) -> str:
+    """
+    Hashes the plain text password.
+
+    Parameters
+    ----------
+    password: str
+        The plain text password to be hashed.
+    Returns
+    -------
+    str
+        The hashed password.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def sign_up():
+    with st.form(key='signUpForProject', clear_on_submit=True):
+        st.subheader('Sign Up')
+        email = st.text_input(':blue[Email]', placeholder='Enter Your Email')
+        username = st.text_input(':blue[Username]', placeholder='Enter Your Username')
+        password1 = st.text_input(':blue[Password]', placeholder='Enter Your Password', type='password')
+        password2 = st.text_input(':blue[Confirm Password]', placeholder='Confirm Your Password', type='password')
+
+        if email:
+            if validate_email(email):
+                if check_in_emails(email):
+                    if validate_username(username):
+                        if check_in_usernames(username):
+                            if len(username) >= 2:
+                                if len(password1)>=6:
+                                    if password1 == password2:
+                                        
+                                        #Adding the user
+                                        # hashed_password = stauth.Hasher(password2).generator();
+                                        # hashed_password = hash(password2)
+                                        insert_user(email,username, hash(password2))
+                                        st.success('Account has been created Successfully!!')
+                                        # st.balloons()
+                                    else:
+                                        st.warning('Passwords do not match')
+                                else:
+                                    st.warning('Password is too short')
+                            else:
+                                st.warning('Username Too Short')
+                        else:
+                            st.warning('Username already exists')
+                    else:
+                        st.warning('Invalid Username')
+                else:
+                    st.warning('Email Already exists')
+            else:
+                st.warning('Invalid Email')
+
+        btn1, btn2, btn3, btn4, btn5 = st.columns(5)
+        with btn3:
+            st.form_submit_button('Sign Up')
+
+sign_up()
---
+++ script.py
@@ -0,0 +1,197 @@
+import pickle
+from docx import Document
+import streamlit as st
+import streamlit_authenticator as stauth
+from dependencies import sign_up, fetch_users, connection
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import google.generativeai as palm
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms.google_palm import GooglePalm
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from htmlTemplates import css,bot_template,user_template
+from dotenv import load_dotenv
+load_dotenv()
+
+#This will generate text from the pdfs, docs uploaded and return the text
+def get_text(docs):
+    text = ""
+    for doc in docs:
+        file_name = doc.name
+        text+= "The file name is %s\n"%file_name
+        if(file_name.endswith('.pdf')):
+            pdf_reader = PdfReader(doc)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        if(file_name.endswith('.docs')):
+            docs_reader = Document(doc)
+            for paragraph in docs_reader.paragraphs:
+                text += paragraph.text + "\n"
+    return text
+
+#This will take the text, create the chunks and return those chunks
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+#This will take GooglePalmEmbeddings, and makes a local vector_store using FAISS
+def get_vector_store(text_chunks):
+    embeddings = GooglePalmEmbeddings()
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vector_store
+
+#This will read the previous text which is in the server
+def read_previous_text(username):
+    data_file = f"data_{username}.obj"
+
+    try:
+        with open(data_file, 'rb') as readFile:
+            previous_txt = pickle.load(readFile)
+        return previous_txt
+    except FileNotFoundError:
+        return ""
+
+#This will write the updated text into the data files in the server
+def write_text_data_file(username,previous_text):
+    data_file = f"data_{username}.obj"
+    with open(data_file, 'wb') as writeFile:
+        pickle.dump(previous_text, writeFile)
+    writeFile.close()
+
+def get_conversational_chain(vector_store):
+    llm=GooglePalm()
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
+
+def submit():
+    st.session_state.user_question = st.session_state.widget
+    st.session_state.widget = ""
+
+def user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chatHistory = response['chat_history']
+    chat_history_length = len(st.session_state.chatHistory)
+
+    # Iterate over chat history in reverse order
+    for i in range(chat_history_length - 2, -1, -2):
+        user_message = st.session_state.chatHistory[i]
+        bot_message = st.session_state.chatHistory[i + 1]
+        st.write(user_template.replace("{{MSG}}", user_message.content), unsafe_allow_html=True)
+        st.write(bot_template.replace("{{MSG}}", bot_message.content), unsafe_allow_html=True)
+
+def main():
+    try:
+        # st.set_page_config(
+        #     page_title="DocPlay 💬",
+        #     layout="wide",
+        # )
+        st.header("DocPlay 💬")
+        users = fetch_users()
+        # users = [
+        #     {'email': 'user1@example.com', 'username': 'user1', 'password': 'password1'},
+        #     {'email': 'user2@example.com', 'username': 'user2', 'password': 'password2'},
+        #     {'email': 'user3@example.com', 'username': 'user3', 'password': 'password3'},
+        #     # Add more users as needed
+        # ]
+        emails = []
+        usernames = []
+        passwords = []
+    
+        for user in users:
+            emails.append(user['email'])
+            usernames.append(user['username'])
+            passwords.append(user['password'])
+        
+        credentials = {'usernames': {}}
+        for index in range(len(emails)):
+            credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
+        
+        Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', cookie_key='jwtsecretkey', cookie_expiry_days=4)
+        # _, authentication_status, username = Authenticator.login(':green[Login]', 'main')
+        _, authentication_status, username = Authenticator.login()
+
+        info, _ = st.columns(2)
+
+        ##check sign up
+        if not authentication_status:
+            # connection()
+            sign_up()
+
+        if username:
+            if username in usernames:
+                if authentication_status:
+                    #let user see the app
+                    st.write(css, unsafe_allow_html=True)
+
+                    st.text_input("Ask a Question from the PDF Files", key="widget", on_change=submit)
+                    if "user_question" not in st.session_state:
+                        st.session_state.user_question = None
+                    user_question = st.session_state.user_question
+                    if "conversation" not in st.session_state:
+                        st.session_state.conversation = None
+                    if "chatHistory" not in st.session_state:
+                        st.session_state.chatHistory = None
+                    if "clear_history_pressed" not in st.session_state:
+                        st.session_state.clear_history_pressed = False
+                    if user_question:
+                        user_input(user_question)
+                    
+
+                    with st.sidebar:
+                        st.title(f'Welcome {username}')
+                        st.subheader("Upload your Documents")
+                        checkbox_container = st.empty()
+                        previous_data = checkbox_container.checkbox("Previous text", disabled = st.session_state.clear_history_pressed)
+
+                        if st.button("Clear History"):
+                            write_text_data_file(username,"")
+                            st.session_state.clear_history_pressed = True
+                        
+                        if previous_data:
+                            if st.button("Process"):
+                                with st.spinner("Processing"):
+                                    previous_txt = read_previous_text(username)
+                                    previous_txt = "\nThis is the previous text\n" + previous_txt
+                                    write_text_data_file(username,previous_txt)
+                                    text_chunks = get_text_chunks(previous_txt)
+                                    vector_store = get_vector_store(text_chunks)
+                                    st.session_state.conversation = get_conversational_chain(vector_store)
+                                    st.success("Done")
+                        else:
+                            pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+                            if st.button("Process"):
+                                with st.spinner("Processing"):
+                                    raw_text = get_text(pdf_docs)
+                                    previous_txt = read_previous_text(username)
+                                    previous_txt = raw_text + "\nThis is the previous text\n" + previous_txt
+                                    write_text_data_file(username,previous_txt)
+                                    st.session_state.clear_history_pressed = False
+                                    text_chunks = get_text_chunks(previous_txt)
+                                    vector_store = get_vector_store(text_chunks)
+                                    st.session_state.conversation = get_conversational_chain(vector_store)
+                                    st.success("Done")
+                        Authenticator.logout('Log Out','sidebar')
+
+                elif not authentication_status:
+                    with info:
+                        st.error('Invalid Username and Password')
+                else:
+                    with info:
+                        st.warning('Please enter credentials')
+            else:
+                with info:
+                    st.warning("Username does not exist, Please Sign up")
+    
+    except Exception as e:  # Catch any type of exception
+        print(e)
+        st.error(f'Error {e}')
+        import traceback
+        traceback.print_exc()
+
+if __name__ == "__main__":
+    data_file = "data.obj"
+    main()
